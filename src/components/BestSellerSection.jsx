c691ac643b1fcc85f83daab833321427505e53dc
nahidraz23 <nzamanraz@gmail.com>
2024-04-30 21:23:37 +0600

    best seller section updated

--- src/components/BestSellerSection.jsx
+++ src/components/BestSellerSection.jsx
@@ -1,90 +1,58 @@
+import { useState } from "react";
 
 const BestSellerSection = () => {
+
+    const data = [
+        {
+            imgelink:
+                "https://images.unsplash.com/photo-1523367662635-ec427c441fe6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNlcmFtaWNzfGVufDB8MHwwfHx8MA%3D%3D",
+        },
+        {
+            imgelink:
+                "https://images.unsplash.com/photo-1530006498959-b7884e829a04?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGNlcmFtaWNzfGVufDB8MHwwfHx8MA%3D%3D",
+        },
+        {
+            imgelink:
+                "https://images.unsplash.com/photo-1554042861-0ad2fc2a8dc2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvcmNlbGFpbnxlbnwwfDB8MHx8fDA%3D",
+        },
+        {
+            imgelink:
+                "https://images.unsplash.com/photo-1613376050219-7182945e9a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBvcmNlbGFpbnxlbnwwfDB8MHx8fDA%3D",
+        },
+        {
+            imgelink:
+                "https://images.unsplash.com/photo-1565972093658-620985899236?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHBvcmNlbGFpbnxlbnwwfDB8MHx8fDA%3D",
+        },
+    ];
+
+    const [active, setActive] = useState(
+        "https://images.unsplash.com/photo-1565972093658-620985899236?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHBvcmNlbGFpbnxlbnwwfDB8MHx8fDA%3D",
+    );
+
     return (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-            <div className="grid gap-4">
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1520408222757-6f9f95d87d5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG90dGVyeXxlbnwwfHwwfHx8MA%3D%3D"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center "
-                        src="https://images.unsplash.com/photo-1605117012605-b68dedd4accc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBvdHRlcnl8ZW58MHx8MHx8fDA%3D"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1597602805046-8f9597eedd6b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9yY2VsYWlufGVufDB8fDB8fHww"
-                        alt="gallery-photo"
-                    />
-                </div>
+        <div>
+            <div className="my-5">
+                <h1 className="text-4xl font-sofia font-bold text-center">Best Seller</h1>
             </div>
             <div className="grid gap-4">
                 <div>
                     <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1618520049060-b851c66e80ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBvcmNlbGFpbnxlbnwwfHwwfHx8MA%3D%3D"
-                        alt="gallery-photo"
+                        className="h-auto w-full max-w-full rounded-lg md:h-[480px]"
+                        src={active}
+                        alt=""
                     />
                 </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1523264067855-7b9941f18ca9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNlcmFtaWNzfGVufDB8fDB8fHww"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center "
-                        src="https://images.unsplash.com/photo-1577576223147-9298630f1c14?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBvcmNlbGFpbnxlbnwwfHwwfHx8MA%3D%3D"
-                        alt="gallery-photo"
-                    />
-                </div>
-            </div>
-            <div className="grid gap-4">
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1610701596007-11502861dcfa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2VyYW1pY3N8ZW58MHx8MHx8fDA%3D"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center "
-                        src="https://images.unsplash.com/photo-1610128361323-6e941c97f023?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGNlcmFtaWNzfGVufDB8fDB8fHww"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1523367438061-01c055ce790c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDJ8fGNlcmFtaWNzfGVufDB8fDB8fHww"
-                        alt="gallery-photo"
-                    />
-                </div>
-            </div>
-            <div className="grid gap-4">
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1605117012605-b68dedd4accc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2VyYW1pY3N8ZW58MHwxfDB8fHww"
-                        alt="gallery-photo"
-                    />
-                </div>
-                <div>
-                    <img
-                        className="h-auto max-w-full rounded-lg object-cover object-center"
-                        src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-                        alt="gallery-photo"
-                    />
+                <div className="grid grid-cols-5 gap-4 justify-items-center">
+                    {data.map(({ imgelink }, index) => (
+                        <div key={index}>
+                            <img
+                                onClick={() => setActive(imgelink)}
+                                src={imgelink}
+                                className="h-20 max-w-full cursor-pointer rounded-lg object-cover object-center"
+                                alt="gallery-image"
+                            />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
